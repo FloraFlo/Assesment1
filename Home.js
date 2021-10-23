b4ede13e9533b38e2490f3557f528531b4273dff
Flora <mazzavillanif@gmail.com>
2021-10-23 13:28:00 +1100

    storage

--- Home.js
+++ Home.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StatusBar, FlatList, Keyboard } from 'react-native';
 import styled from 'styled-components';
 import AddInput from './Components/AddInput';
 import TodoList from "./Components/TodoList"; 
 import Empty from "./Components/Empity";
 import Header from './Components/Header';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function HomePage() {
 
   const [data, setData] = useState([]);
+
+  const [appInit, setAppInit] = useState(true);
+
+  useEffect(() => {
+    if (appInit) {
+      getData();
+      setAppInit(false);
+      console.log("getting data...");
+    } else {
+      storeData();
+      console.log("Storing data....");
+    }
+    storeData();
+  }, [data]);
 
   const submitHandler = (value) => {
     setData ((prevTodo) => {
@@ -27,6 +42,24 @@ export function HomePage() {
       return prevTodo.filter((todo) => todo.key != key);
     });
   }; 
+
+  const storeData = async () => {
+    const stringified = JSON.stringify(data);
+    try {
+      await AsyncStorage.setItem("listData", stringified);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const stringified = await AsyncStorage.getItem("listData");
+      setItemArray(stringified !== null ? JSON.parse(stringified) : []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <ComponentContainer>
